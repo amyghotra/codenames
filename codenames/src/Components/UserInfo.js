695eea7c475764bb118cf4218c34bc379aab8b0d
Kevin <kersv@DESKTOP-9KCNLFV.localdomain>
2021-09-19 00:55:14 -0400

    Kevin

--- codenames/src/Components/UserInfo.js
+++ codenames/src/Components/UserInfo.js
@@ -5,6 +5,16 @@ function UserInfo(){
     return(
         <div className="userInfo">
             <h6 className="gameCode"> Game Code: 00000 </h6>
+            <form className="box-userInfo">
+                <label className="prompt">Enter Nickname</label><br/>
+                <input className="Nickname" type="text" placeholder="Nickname" spellCheck="false"></input>
+            </form>
+            <br/>
+            <form className="box-userInfo">
+                <label className="prompt">Choose a team</label><br/>
+                
+            </form>
+    
         </div>
     )
 }
