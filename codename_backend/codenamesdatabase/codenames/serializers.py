3a36fc70a19645e1fb4e41fa3789e45363058675
Kevin <kersv@Kevins-MBP.fios-router.home>
2021-09-25 19:11:05 -0400

    userInfo Kevin

--- codename_backend/codenamesdatabase/codenames/serializers.py
+++ codename_backend/codenamesdatabase/codenames/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Room
+from .models import Room, UserInfo
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ['id', 'room_key']
+
+
+class UserInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserInfo
+        fields = ['id', 'nickname', 'team']
--- codename_backend/codenamesdatabase/codenames/urls.py
+++ codename_backend/codenamesdatabase/codenames/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('codenames/', views.RoomList.as_view()),
+    path('codenames/userInfo', views.UserInfoList.as_view()),
 ]
