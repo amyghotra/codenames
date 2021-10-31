d8de8c11cc953718cd09cadb21e5bb69bc8d236b
Daniel.Rizzo30 <rimshot816@DESKTOP-2UTBUI6.localdomain>
2021-10-31 12:06:08 -0400

    CLUEBOX CHANNEL WORKING

--- codename_backend/codenamesdatabase/codenames/consumers.py
+++ codename_backend/codenamesdatabase/codenames/consumers.py
@@ -2,46 +2,44 @@
 # define ws consumer class here
 
 import json
-import asyncio
+import asyncio # can remove
+from asgiref.sync import async_to_sync # New 
 from channels.consumer import AsyncConsumer
+from channels.generic.websocket import WebsocketConsumer # Can remove
 
-class ClueBoxConsumer(AsyncConsumer):
-    async def websocket_connect(self, event):
-        print("connected", event)
-        await self.send({
-            "type": "websocket.accept"
-        })
-        
+class ClueBoxConsumer(WebsocketConsumer):
+    def connect(self):
         """
-        Connect to room
+        Connect to a chat room
         Spaces are replaced like this: 'My new room' -> 'My_new_room'
         """
-        
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_name = self.room_name.replace(' ', '_')
-        self.room_group_name = 'cluebox_%s' % self.room_name # starts with cluebox
+        self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-    async def websocket_disconnect(self, close_code):
-        await self.channel_layer.group_discard(
+        self.accept()
+        print("added to group")
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
-    async def websocket_receive(self, text_data):
+    def receive(self, text_data):
         """
         Receive a message and broadcast it to a room group
         UTC time is included so the client can display it in each user's local time
-        """
-        front_text = text_data.get('text', None)
-        text_data_json = json.loads(front_text)
+        """   
+        text_data_json = json.loads(text_data)
+        print(text_data_json)
         count = text_data_json['count']
         clue = text_data_json['clue']
-        # broadcasts the message
-        await self.channel_layer.group_send(
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 "type": "spymasterClue", # same as message defined below
@@ -50,7 +48,7 @@ class ClueBoxConsumer(AsyncConsumer):
             }
         )
 
-    async def spymasterClue(self, event): # must be same as 'type': 'message', ~6 lines above
+    def spymasterClue(self, event): # must be same as 'type': 'spymasterClue', ~6 lines above
         """
         Receive a broadcast message and send it over a websocket
         """
@@ -58,9 +56,81 @@ class ClueBoxConsumer(AsyncConsumer):
         count = event['count']
         clue = event['clue']
 
-        # Send message to WebSocket
-        await self.send({
-            'type': 'websocket.send',
+        self.send(text_data=json.dumps({
             'count': count,
             'clue': clue,
-        })
+        }))
+
+        print("test") # Each instance of the socket should print this out
+
+# class ClueBoxConsumer(AsyncConsumer): # Not working use above
+#     async def websocket_connect(self, event):
+#         print("connected", event)
+#         await self.send({
+#             "type": "websocket.accept"
+#         })
+        
+#         """
+#         Connect to room
+#         Spaces are replaced like this: 'My new room' -> 'My_new_room'
+#         """
+        
+#         self.room_name = self.scope['url_route']['kwargs']['room_name']
+#         self.room_name = self.room_name.replace(' ', '_')
+#         self.room_group_name = 'cluebox_%s' % self.room_name # starts with cluebox
+
+#         # Join room group
+#         await self.channel_layer.group_add(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+#         print("added to group")
+
+#     async def websocket_disconnect(self, close_code):
+#         await self.channel_layer.group_discard(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+#     async def websocket_receive(self, text_data):
+#         """
+#         Receive a message and broadcast it to a room group
+#         UTC time is included so the client can display it in each user's local time
+#         """
+#         print("receive", self)
+#         front_text = text_data.get('text', None)
+#         text_data_json = json.loads(front_text)
+#         print(text_data_json)
+#         count = text_data_json['count']
+#         clue = text_data_json['clue']
+
+#         # broadcasts the message
+#         await self.channel_layer.group_send(
+#             self.room_group_name,
+#             {
+#                 "type": "spymasterClue", # same as message defined below
+#                 "count": count,
+#                 "clue": clue,
+#             }
+#         )
+
+#     async def spymasterClue(self, event): # must be same as 'type': 'spymasterClue', ~6 lines above
+#         """
+#         Receive a broadcast message and send it over a websocket
+#         """
+        
+#         count = event['count']
+#         clue = event['clue']
+        
+#         # Send message to WebSocket
+#         await self.send({
+#             'type': "websocket.send",
+#             'count': count,
+#             'clue': clue,
+#         })
+
+#         # await self.send(text_data=json.dumps({
+#         #     'count': count,
+#         #     'clue': clue,
+#         # }))
+
+#         print("test") # Each instance of the socket should print this out
