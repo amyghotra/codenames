44769235e953d775a67540de85d8f086ba534632
Amy Ghotra <amy.ghotra8@gmail.com>
2021-12-11 19:32:40 -0500

    edited DBs

--- codenamesdatabase/local_settings.py
+++ codenamesdatabase/local_settings.py
@@ -5,16 +5,5 @@ SECRET_KEY = 'k@)rec)_yu&0z2hmey#8j^rs-3-0pd=nc%zg7=z#ra9q5(6+if'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'codenames_database',
-        'USER': 'root',
-        # Hidden Password s
-        'PASSWORD': 'P@ssw0rd',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'autocommit': True
-        }
-    }
+    
 }
