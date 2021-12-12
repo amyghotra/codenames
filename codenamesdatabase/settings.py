414381b7b884821d7e5a0710d62f0941095e9e97
MichaelTessey1 <michaeltessey1@gmail.com>
2021-12-11 21:50:09 -0500

    added urls to allowed host

--- codenamesdatabase/settings.py
+++ codenamesdatabase/settings.py
@@ -17,10 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 1
-ALLOWED_HOSTS=['*']
+ALLOWED_HOSTS=['codenames21-backend.herokuapp.com', '127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL = True
 
 
