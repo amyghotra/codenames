e943ea1a34748346f72dd24f2bdc5ad990d7dc16
Amy Ghotra <amy.ghotra8@gmail.com>
2021-12-13 22:50:44 -0500

    Update serializers.py

--- codenames/serializers.py
+++ codenames/serializers.py
@@ -73,8 +73,6 @@ class GameSerializer(serializers.ModelSerializer):
             GameWords.objects.create(game_id=game, word=word, category='C')
 
         GameWords.objects.create(game_id=game, word=words_data[24], category='A')
-        
-        random.shuffle(gameWords)
 
         return game
 
